5d37d7d8d7b71737cd6b595b2a91eac6d724536b
iparker3964 <iparker3964@yahoo.com>
2023-05-11 15:58:43 -0500

    Completion

---
+++ MovieProDemo/Controllers/ActorController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieProDemo.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieProDemo.Services.Interfaces;
+
+namespace MovieProDemo.Controllers
+{
+    public class ActorController : Controller
+    {
+        private readonly IDataMappingService _tmdbMappingService;
+        private readonly IRemoteMovieService _tmdbMovieService;
+        public ActorController(IRemoteMovieService tmdbMovieService, IDataMappingService tmdbMappingService)
+        {
+            _tmdbMappingService = tmdbMappingService;
+            _tmdbMovieService = tmdbMovieService;
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            var actor = await _tmdbMovieService.ActorDetailAsync(id);
+            actor = _tmdbMappingService.MapActorDetail(actor);
+            return View(actor);
+        }
+    }
+}
---
+++ MovieProDemo/Models/Settings/QueryOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieProDemo.Models.Settings
+{
+    public class QueryOptions
+    {
+        public string Language { get; set; }
+        public string AppendToResponse { get; set; }
+        public string Page { get; set; }
+    }
+}
